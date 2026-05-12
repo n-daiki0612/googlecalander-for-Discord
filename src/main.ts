01984595b965055063e9eb066d43989100f0245d
n-daiki0612 <n.daiki.00661122@gmail.com>
2026-05-12 09:16:35 +0900

    通知実装

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-function testLog(): void {
-  console.log("Hello  TypeScript!");
+import { CONFIG } from './config.js';
+
+export const messagesend = { 
+
+  send(content: string){
+    const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+      method:"post",
+      contentType: "application/json",
+      payload: JSON.stringify({ content: content })
+    }
+
+  
+  return UrlFetchApp.fetch(CONFIG.WEBHOOK_URL,options);
+  }
 }
 
+function message(){
+messagesend.send("test")
+}
+
+(globalThis as any).message = message;
 
